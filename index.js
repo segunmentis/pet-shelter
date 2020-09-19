f4f5ae5c60d37b9409d53bb219c95891460f1dc3
babasegun_adeleye <mentis2010@gmail.com>
2020-09-19 09:02:24 -0600

    Add mysql body-parser

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
 
+const datas = require("./data.json");
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send(datas);
 });
 
-app.listen(port, () => {
-    console.log("App is listenin on port: ", port);
+app.get("/pet/:id", (req, res) => {
+    const pets = datas.find(p => p.id === parseInt(req.params.id));
+    if(!pets) res.status(404).send("Data not found");
+    res.send(pets);
 });
+
+
+app.listen(port);
