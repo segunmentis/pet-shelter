6d02513ca8b7706b87ca65ba637b523084344a7e
babasegun_adeleye <mentis2010@gmail.com>
2020-09-23 15:33:58 -0600

    update config

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: 'nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    USER: 'qpa298x2my3bd1mi',
-    PASSWORD: 'e7gx75fni7db766s',
-    DB: 'r49iu1vcf0kdyl2n',
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: '',
+    DB: 'shelter',
 }
