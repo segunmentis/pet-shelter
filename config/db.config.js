93c2d46e06fcb339ab1011399d253164fe8c5b36
babasegun_adeleye <mentis2010@gmail.com>
2020-09-23 02:08:59 -0600

    Config changes

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "shelter"
+    HOST: 'us-cdbr-east-02.cleardb.com',
+    USER: 'bf7c434e4f1a4e',
+    PASSWORD: 'e4e03d29',
+    DB: 'heroku_ee64ea50f81fb96',
 }
